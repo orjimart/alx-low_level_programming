bb211a477aad384efc9a21fc721298ccd10ca0a3
orjimart <orjimart@gmail.com>
2023-09-07 18:15:02 +0100

    using printf to display to stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - using printf to print to stdout
+ * Description - print with proper grammer, but the outcome is a piece of art
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
