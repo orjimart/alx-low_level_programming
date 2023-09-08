c45528363e0fd4de2501e8b45c4e6a50aeb1cedd
orjimart <orjimart@gmail.com>
2023-09-08 03:09:35 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - no puts or printf
- * 
- * Return:0
+ *
+ * Return: 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) ;
-	return (1) ;
-}	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
