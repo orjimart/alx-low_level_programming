f14b38c1f5d2432b0616d451a245c608b8f6b0dd
orjimart <orjimart@gmail.com>
2023-09-09 20:41:46 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,24 @@
  */
 int main(void)
 {
-	int n, m;
+	int n, m, a;
 
 	for (n = 48; n <= 56; n++)
 	{
 	for (m = 49; m <= 57; m++)
 	{
-	if (m > n)
+	for (a = 50; a < 58; a++)
+	{
+	if (a > m && m > n)
 	{
 	putchar(n);
 	putchar(m);
+	putchar(a);
 	if (n != 56 || m != 57)
 	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
